6f7300103cf9adc38873ca9446d7dc003eb0eef3
Lalit Kumar <lalit.modi@datenwissen.com>
2023-08-24 19:25:48 +0530

    remove unusable files

--- pages/post.js
+++ pages/post.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Carousel2 from '../components/Carousel'
+
 import { useState, useEffect } from 'react'
 import Layout from '../components/Layout'
 const post = () => {
