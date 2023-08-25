76ec5e905f0d4214f2b2b19c2e0219497079db80
Lalit Kumar <lalit.modi@datenwissen.com>
2023-08-25 18:20:48 +0530

    staff page added

--- components/Authcheck.js
+++ components/Authcheck.js
@@ -40,7 +40,7 @@ function RouteGuard({ children }) {
 
     function authCheck(url) {
         // redirect to login page if accessing a private page and not logged in 
-        const publicPaths = ['/login','/Signup'];
+        const publicPaths = ["/login", "/Signup", "/staffpage"];
         const path = url.split('?')[0];
         if (!userAuth && !publicPaths.includes(path) && !staffAuth) {
           setAuthorized(false);
---
+++ pages/staffpage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+
+const staffpage = () => {
+  return (
+    <Layout>
+    <div>
+
+        <h2>StaffPage screen</h2>
+    </div>
+    </Layout>
+  )
+}
+
+export default staffpage
